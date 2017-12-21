c03face4d45947037d5f27a535e5a46a841d8451
keven chen <keven_chen@htc.com>
2017-12-21 13:26:51 +0800

    don't copy ssh file, if it already exits

--- repo/bin/init.sh
+++ repo/bin/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod 777 $HOME/.ssh &&
-if [ -f $HOME/tmp//id_rsa ]; then mv $HOME/tmp/* $HOME/.ssh;  else echo "no file id_rsa" ; fi &&
+if [ -f $HOME/tmp/id_rsa ] && [ ! -f $HOME/.ssh/id_rsa ]; then mv $HOME/tmp/* $HOME/.ssh;  else echo "don't copy file id_rsa"; fi &&
 chmod 777 -R $HOME/code &&
 echo -e $HOST_NAME >> /etc/hosts &&
 git config --global user.name "$GIT_NAME" &&
